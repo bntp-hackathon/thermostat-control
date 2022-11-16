45b1bc015b7478259666fb383f5b4997ac5c6d16
KrawGleb <krawcevitsch@gmail.com>
2022-11-16 19:17:16 +0300

    Add navigation

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import NavigationCompanent from '@react-navigation/native'
-import Ionic from 'react-native-vector-icons'
-
-
+import NavigationCompanent from "@react-navigation/native";
+import Ionic from "react-native-vector-icons";
+import MainContainer from "./app/components/navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, React native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MainContainer />;
 }
 
 const styles = StyleSheet.create({
