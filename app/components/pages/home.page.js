cd01d56d6e3da92e7fa6c0d4bc2f9dcf46d02a28
KrawGleb <krawcevitsch@gmail.com>
2022-11-16 19:43:10 +0300

    Implement home page

--- app/components/pages/home.page.js
+++ app/components/pages/home.page.js
@@ -1,17 +1,33 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, Button, StyleSheet, Alert } from "react-native";
 
 function HomePage({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => alert('This is the "Home" screen.')}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Home Screen
+      <Image
+        style={{ width: 250, height: 250 }}
+        source={require("../../../assets/empty.png")}
+      />
+
+      <Text style={{ fontSize: 20, color: "gray" }}>
+        Устройства отсутствуют
       </Text>
+
+      <View style={{ margin: 10 }}>
+        <Button
+          title="Добавить устройство"
+          color="#0879cf"
+          onPress={() => Alert.alert("Button with adjusted color pressed")}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    marginVertical: 20,
+  },
+});
 
 export default HomePage;
